04e1110a175bbbaed48dec8ae06b5cc8922839ad
Phạm Việt Hoàng <pvhoang270201@gmail.com>
2026-01-12 15:18:22 +0700

    2

--- c1/b2/bai2.js
+++ c1/b2/bai2.js
@@ -1,5 +1,14 @@
 class Person {
-  constructor(name) {
+  constructor(name, address) {
     this.name = name;
+    this.address = address;
   }
+  getAddress() {
+    return "i live in " + this.address;
+  }
+
 }
+
+const test = new Person("Alice", "123 Main St");
+console.log(test);
+console.log(test.getAddress());
